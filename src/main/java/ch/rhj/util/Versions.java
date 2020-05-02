73a2cc06647f9f8af45398aceb2c4faafb78da2d
rhjoerg <rhjoerg@gmail.com>
2020-05-02 21:42:52 +0200

    initial import

---
+++ src/main/java/ch/rhj/util/Versions.java
@@ -0,0 +1,33 @@
+package ch.rhj.util;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Versions {
+
+	public static <T> VersionComparator<T> comparator(Function<T, String> mapper) {
+
+		return VersionComparator.<T>comparator(mapper);
+	}
+
+	public static int compare(String v1, String v2) {
+
+		return comparator(Function.identity()).compare(v1, v2);
+	}
+
+	public static <T> int compare(Function<T, String> mapper, T t1, T t2) {
+
+		return comparator(mapper).compare(t1, t2);
+	}
+
+	public static <T> Stream<T> sorted(Function<T, String> mapper, Stream<T> input) {
+
+		return input.sorted(comparator(mapper));
+	}
+
+	public static <T> Optional<T> latest(Function<T, String> mapper, Stream<T> input) {
+
+		return input.max(comparator(mapper));
+	}
+}
---
+++ src/test/java/ch/rhj/util/VersionsTests.java
@@ -0,0 +1,74 @@
+package ch.rhj.util;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Test;
+
+public class VersionsTests {
+
+	public static class TestVersion {
+
+		public final String version;
+
+		public TestVersion(String version) {
+
+			this.version = version;
+		}
+	}
+
+	private TestVersion testVersion(String version) {
+
+		return new TestVersion(version);
+	}
+
+	private final Function<TestVersion, String> mapper = v -> v.version;
+
+	@Test
+	public void testCtor() {
+
+		new Versions();
+	}
+
+	@Test
+	public void testStringVersions() {
+
+		String v1 = "1.0.0";
+		String v2 = "1.1.0";
+		String v3 = "1.10.0";
+
+		assertTrue(Versions.compare(v1, v2) < 0);
+		assertTrue(Versions.compare(v2, v3) < 0);
+
+		String v4 = v1 + "-snapshot";
+
+		assertTrue(Versions.compare(v4, v1) < 0);
+
+		String v5 = v1 + "-alpha";
+
+		assertTrue(Versions.compare(v5, v1) < 0);
+	}
+
+	@Test
+	public void testComplexVersions() {
+
+		TestVersion v1 = testVersion("1.0.0");
+		TestVersion v2 = testVersion("1.1.0");
+		TestVersion v3 = testVersion("1.1.0-alpha");
+
+		assertTrue(Versions.compare(mapper, v1, v2) < 0);
+
+		List<TestVersion> versions = Arrays.asList(v1, v2, v3);
+		List<TestVersion> expected = Arrays.asList(v1, v3, v2);
+		List<TestVersion> actual = Versions.sorted(mapper, versions.stream()).collect(toList());
+
+		assertEquals(expected, actual);
+
+		assertTrue(v2 == Versions.latest(mapper, versions.stream()).get());
+	}
+}
