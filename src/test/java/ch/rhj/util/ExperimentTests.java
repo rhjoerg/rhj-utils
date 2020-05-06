357c888f37d7bc0b58895900d7aed92c7845dd30
rhjoerg <rhjoerg@gmail.com>
2020-05-06 09:23:52 +0200

    deployment doesn't work yet.

--- src/test/java/ch/rhj/util/ExperimentTests.java
+++ src/test/java/ch/rhj/util/ExperimentTests.java
@@ -1,12 +1,4 @@
 package ch.rhj.util;
 
-import org.junit.jupiter.api.Test;
-
 public class ExperimentTests {
-
-	@Test
-	public void experiment1() {
-
-		System.out.println("---- RUNNING AS: '" + SysProps.userName() + "' ----");
-	}
 }
